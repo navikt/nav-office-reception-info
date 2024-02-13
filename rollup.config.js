2b67e1a6d5fe35c0f53164356d2e60e30a612fea
Bjørn Harbo Dahle <bdahle@hey.com>
2024-02-13 14:24:01 +0100

    første utkast config

---
+++ rollup.config.js
@@ -0,0 +1,48 @@
+import { babel } from "@rollup/plugin-babel";
+import typescript from "rollup-plugin-typescript2";
+import { nodeResolve } from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+
+// import commonjs from "@rollup/plugin-commonjs";
+// import npmImport from "less-plugin-npm-import";
+// import external from "rollup-plugin-peer-deps-external";
+// import postcss from "rollup-plugin-postcss";
+// import resolve from "@rollup/plugin-node-resolve";
+// import json from "@rollup/plugin-json";
+// import image from "@rollup/plugin-image";
+// import url from "@rollup/plugin-url";
+
+// import svgr from "@svgr/rollup";
+import pkg from "./package.json" assert { type: "json" };
+
+export default {
+  input: "src/Reception.tsx",
+  output: [
+    {
+      file: pkg.main,
+      format: "cjs",
+      exports: "named",
+      sourcemap: true,
+    },
+    {
+      file: pkg.module,
+      format: "es",
+      exports: "named",
+      sourcemap: true,
+    },
+  ],
+
+  plugins: [
+    nodeResolve(),
+    commonjs(),
+    typescript({
+      tsconfig: "tsconfig.json",
+      jsx: "react",
+    }),
+    babel({
+      //   babelHelpers: "runtime", TODO
+      babelHelpers: "bundled",
+      extensions: [".js", ".jsx", ".ts", ".tsx"],
+    }),
+  ],
+};
