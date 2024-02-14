35ac1e29dd341a286d5a0fc7fa36440161b80c64
Bjørn Harbo Dahle <bdahle@hey.com>
2024-02-14 15:08:18 +0100

    Revert "legg til libInjectCss"
    This reverts commit 7e298aa92ba8efdcf72b30d06de9b74a51fd9a6d.

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,11 @@
 import { defineConfig } from 'vite';
 import { resolve } from 'path';
 import react from '@vitejs/plugin-react-swc';
-import { libInjectCss } from 'vite-plugin-lib-inject-css';
 import dts from 'vite-plugin-dts';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react(), libInjectCss(), dts({ include: ['lib'] })],
+    plugins: [react(), dts({ include: ['lib'] })],
     build: {
         lib: {
             entry: resolve(__dirname, 'lib/main.ts'),
