7e298aa92ba8efdcf72b30d06de9b74a51fd9a6d
Bjørn Harbo Dahle <bdahle@hey.com>
2024-02-14 13:21:04 +0100

    legg til libInjectCss

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
 import { defineConfig } from 'vite';
 import { resolve } from 'path';
 import react from '@vitejs/plugin-react-swc';
+import { libInjectCss } from 'vite-plugin-lib-inject-css';
 import dts from 'vite-plugin-dts';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react(), dts({ include: ['lib'] })],
+    plugins: [react(), libInjectCss(), dts({ include: ['lib'] })],
     build: {
         lib: {
             entry: resolve(__dirname, 'lib/main.ts'),
