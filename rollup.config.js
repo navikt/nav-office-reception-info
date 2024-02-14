54b4097a6cab91acb54bd878a61ea15edd06951c
Bjørn Harbo Dahle <bdahle@hey.com>
2024-02-14 08:59:21 +0100

    prettify noen filer

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,7 @@
-import { babel } from "@rollup/plugin-babel";
-import typescript from "rollup-plugin-typescript2";
-import { nodeResolve } from "@rollup/plugin-node-resolve";
-import commonjs from "@rollup/plugin-commonjs";
+import { babel } from '@rollup/plugin-babel';
+import typescript from 'rollup-plugin-typescript2';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
 
 // import commonjs from "@rollup/plugin-commonjs";
 // import npmImport from "less-plugin-npm-import";
@@ -13,36 +13,36 @@ import commonjs from "@rollup/plugin-commonjs";
 // import url from "@rollup/plugin-url";
 
 // import svgr from "@svgr/rollup";
-import pkg from "./package.json" assert { type: "json" };
+import pkg from './package.json' assert { type: 'json' };
 
 export default {
-  input: "src/Reception.tsx",
-  output: [
-    {
-      file: pkg.main,
-      format: "cjs",
-      exports: "named",
-      sourcemap: true,
-    },
-    {
-      file: pkg.module,
-      format: "es",
-      exports: "named",
-      sourcemap: true,
-    },
-  ],
+    input: 'src/Reception.tsx',
+    output: [
+        {
+            file: pkg.main,
+            format: 'cjs',
+            exports: 'named',
+            sourcemap: true,
+        },
+        {
+            file: pkg.module,
+            format: 'es',
+            exports: 'named',
+            sourcemap: true,
+        },
+    ],
 
-  plugins: [
-    nodeResolve(),
-    commonjs(),
-    typescript({
-      tsconfig: "tsconfig.json",
-      jsx: "react",
-    }),
-    babel({
-      //   babelHelpers: "runtime", TODO
-      babelHelpers: "bundled",
-      extensions: [".js", ".jsx", ".ts", ".tsx"],
-    }),
-  ],
+    plugins: [
+        nodeResolve(),
+        commonjs(),
+        typescript({
+            // tsconfig: 'tsconfig.json',
+            // jsx: 'react',
+        }),
+        babel({
+            //   babelHelpers: "runtime", TODO
+            babelHelpers: 'bundled',
+            extensions: ['.js', '.jsx', '.ts', '.tsx'],
+        }),
+    ],
 };
