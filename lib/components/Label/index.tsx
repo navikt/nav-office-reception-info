b2466d3b5dfa587507afda010b9ed4d6e49b222e
Bjørn Harbo Dahle <bdahle@hey.com>
2024-02-16 13:02:20 +0100

    fjern eksempelkomponenter

--- lib/components/Label/index.tsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-export function Label(props: React.LabelHTMLAttributes<HTMLLabelElement>) {
-    return <label {...props} />;
-}
--- lib/main.ts
+++ lib/main.ts
@@ -1,4 +1,1 @@
-export { Button } from './components/Button';
-export { Input } from './components/Input';
-export { Label } from './components/Label';
 export { Reception } from './components/Reception';
