f1eff8ef5e6dfaedbdc18101c3b1b94b501d6012
Bjørn Harbo Dahle <bdahle@hey.com>
2024-04-09 08:48:02 +0200

    test npm i uten Tabs

--- lib/components/Reception/Reception.tsx
+++ lib/components/Reception/Reception.tsx
@@ -1,5 +1,4 @@
-import { useState } from 'react';
-import { BodyLong, Tabs } from '@navikt/ds-react';
+import { BodyLong } from '@navikt/ds-react';
 import { AudienceReception } from '../../utils/types.ts';
 import { SingleReception } from '../SingleReception/SingleReception.tsx';
 import { translator } from '../../utils/translations.ts';
@@ -19,20 +18,6 @@ export const Reception = ({ receptions, language }: Props) => {
     const languageValidated = validateLanguage(language);
     const getOfficeTranslations = translator('office', languageValidated);
 
-    const getLocation = (reception: AudienceReception) => {
-        if (!reception) {
-            return '(Ukjent sted)';
-        }
-        return reception.stedsbeskrivelse || reception.besoeksadresse?.poststed || '(Ukjent sted)';
-    };
-
-    const getIdFromLabel = (label: string) => {
-        return label.replace(/\s/g, '-').toLowerCase();
-    };
-
-    const firstLocation = getLocation(receptions[0]);
-    const [state, setState] = useState(getIdFromLabel(firstLocation));
-
     if (!receptions || receptions.length === 0) {
         return null;
     }
@@ -48,22 +33,6 @@ export const Reception = ({ receptions, language }: Props) => {
     return (
         <>
             <BodyLong className={style.chooseBetweenOffices}>{getOfficeTranslations('chooseBetweenOffices')}</BodyLong>
-            <Tabs value={state} onChange={setState} className={style.officeTabs}>
-                <Tabs.List>
-                    {receptions.map((loc: AudienceReception, index) => {
-                        const locationLabel = getLocation(loc);
-                        return <Tabs.Tab key={index} value={getIdFromLabel(locationLabel)} label={locationLabel} />;
-                    })}
-                </Tabs.List>
-                {receptions.map((loc: AudienceReception, index) => {
-                    const locationLabel = getLocation(loc);
-                    return (
-                        <Tabs.Panel key={index} value={getIdFromLabel(locationLabel)} className={style.singleTab}>
-                            <SingleReception {...loc} language={languageValidated} />
-                        </Tabs.Panel>
-                    );
-                })}
-            </Tabs>
         </>
     );
 };
