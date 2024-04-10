4890ac9ae42b12edf04d83c9246a11f77cb9499b
Bjørn Harbo Dahle <bdahle@hey.com>
2024-04-10 09:25:17 +0200

    export Address and OpeningHours

--- lib/main.ts
+++ lib/main.ts
@@ -1,2 +1,2 @@
 export { Reception } from './components/Reception/Reception';
-export type { AudienceReception } from './utils/types';
+export type { AudienceReception, Address, OpeningHours } from './utils/types';
